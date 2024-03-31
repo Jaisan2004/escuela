efcfa646f11e17ab4794989b2c8531d0f370ba0b
Sebastian Ballen Loaiza <sballen@uniempresarial.edu.co>
2024-03-30 19:05:42 -0500

    cambio

--- colegio/src/app/app.routes.ts
+++ colegio/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
 
 export const routes: Routes = [{path: '', redirectTo: "/login", pathMatch : "full"},
-{path:'login', component:LoginComponent}];
+{path:'login', component:LoginComponent},{path:'inicio', component:InicioComponent}];
