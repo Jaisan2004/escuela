f2ac7ce4f68250e2aaa9f90816c2d1ddb95bb1f4
Sofia Alejandra Cárdenas Tovar <scardenas@uniempresarial.edu.co>
2024-04-01 07:53:17 -0500

    cambios

--- colegio/src/app/app.routes.ts
+++ colegio/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
 
 export const routes: Routes = [{path: '', redirectTo: "/login", pathMatch : "full"},
-{path:'login', component:LoginComponent},{path:'inicio', component:InicioComponent}];
+{path:'login', component:LoginComponent},{path:'inicio', component:InicioComponent},{path:'formulario', component:FormularioComponent}];
--- colegio/src/app/componentes/formulario/formulario.component.ts
+++ colegio/src/app/componentes/formulario/formulario.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { InicioComponent } from '../inicio/inicio.component';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [InicioComponent],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
