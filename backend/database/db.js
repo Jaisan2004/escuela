8bd59f78fb19f52fec736817d557e82f404a06e0
Sebastian Ballen Loaiza <sballen@uniempresarial.edu.co>
2024-04-04 10:20:02 -0500

    cambio

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 require('dotenv').config({path: './.env'});
 
 const connection = mysql.createConnection({
-    user: process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_NAME, 
-    database : process.env.DB_DATABASE, 
+    user: 'root',
+    password : '',
+    host: 'localhost',
+    port: '3306', 
+    database : 'colegio', 
   });
   
 // Conexión a la base de datos
